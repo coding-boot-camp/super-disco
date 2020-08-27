379e545548788bdfc801df008102b12d0e84ab82
Kareem <k2gees@gmail.com>
2020-08-27 18:18:26 -0500

    visuals updated

---
+++ Develop/script.js
@@ -0,0 +1,34 @@
+$(function(){
+    var today = new Date();
+    $("#currentDay").text(today);
+   
+});
+
+$(document).ready(function() {
+    var currentHour = moment().hour();
+    
+    $(".time-block").each(function(){
+        var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+       
+        if (timeBlockHour < currentHour){
+            $(this).children('textarea').addClass("past")
+
+        }else if (timeBlockHour == currentHour){
+            $(this).removeClass("past")
+            $(this).children('textarea').addClass("present")
+
+        } else if (timeBlockHour > currentHour){ 
+            $(this).removeClass("present")
+            $(this).children('textarea').addClass("future")
+
+
+        }
+        //else if and else, when 
+       // timeBlockHour = currentHour : removeclass, adClass
+      // $(this).removeClass("present")
+
+        //timeBlockHour< currentHour
+    })
+;
+    
+});
