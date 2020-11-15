395fc0e98e7df7eee9c3d3d3df14950db0e34f39
Veronica Martinez <vmartinez6615@gmail.com>
2020-11-14 23:08:46 -0500

    made function skeleton

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,24 +1,43 @@
 
-// get the current day and display it on the top of the calendar
+// displays current date at top of calender
 
 var currentDay = document.querySelector("#currentDay");
 
 currentTime = moment();
 
-currentDay.textContent = currentTime.format("dddd, MMMM Do, YYYY, hh: mm");
+currentDay.textContent = currentTime.format("dddd, MMMM Do, YYYY - hh: mm");
 
 // select all time blocks, which will also give us acces to the children 
 
-var timeBlocks = Array.from(document.querySelectorAll(".time-block"))
+var timeBlocks = Array.from(document.querySelectorAll(".time-block"));
 
 var textareaArray = Array.from(document.querySelectorAll(".textarea"));
 
 var currentHour = moment().hour();
 
- var tasks = [loadTasks];
+ var tasks = [];
+
+ var hourUpdater = function(){
+    
+  
+};
+
+hourUpdater();
 
 
+//Declare a 'tasks' variable that holds the parsed task items retrieved from 'localStorage'
+// If there is nothing in 'localStorage', set the 'tasks' to an empty array
 
+var loadTasks = function() {
 
+};
 
-   
+loadTasks();
+//  when the user clicks the save icon tasks are saved to local storage  
+
+$(".saveBtn").on("click", function() {
+
+     
+
+});
+
