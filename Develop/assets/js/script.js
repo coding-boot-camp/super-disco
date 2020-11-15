9b2e70d9db3788e621273166142d8fa1e7fc7c82
Veronica Martinez <vmartinez6615@gmail.com>
2020-11-15 11:59:27 -0500

    working on local storage

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -21,6 +21,44 @@ var currentHour = moment().hour();
     
     // loop over each one timeBlock 
 
+    for (var i = 0; i < timeBlocks.length; i++){
+        // select the hour id and loop through 
+        var timeBlockId =timeBlocks[i].firstElementChild.id;
+
+   //  loop through the time block id and get the string value of hour (get rid of "hour-" by replacing with an empty string)
+
+   var timeBlockHour = timeBlockId.replace("hour-", '')
+
+   // convert to mility time by adding + 12  when needed and then turn timeBlockHours into integer values  
+   if (timeBlockHour < currentHour) {
+
+       timeBlockHour = parseInt(timeBlockHour) + 12
+
+       var presentTimeBlock = document.getElementById(JSON.stringify(timeBlockHour));
+
+       $(presentTimeBlock).addClass("past");
+   }
+
+   else {
+       timeBlockHour = parseInt(timeBlockHour)
+   }
+
+   if (timeBlockHour === parseInt(currentHour)){
+       // addClass "present" to corresponding text area
+
+       var presentTimeBlock = document.getElementById(JSON.stringify(timeBlockHour));
+       
+       $(presentTimeBlock).addClass("present");
+   }
+   else if (timeBlockHour > currentHour) {
+   
+       // addClass "future" to corresponding text area 
+
+       var presentTimeBlock = document.getElementById(JSON.stringify(timeBlockHour));
+       
+       $(presentTimeBlock).addClass("future");
+   }
+}
 };
 
 hourUpdater();
@@ -54,4 +92,13 @@ $(textareaArray[i]).text(tasks[i]);
 loadTasks();
 //  when the user clicks the save icon tasks are saved to local storage  
 
+$(".saveBtn").on("click", function() {
+
+   var userInput = $(this).siblings(".textarea").val()
+
+   tasks.push(userInput);
+
+   localStorage.setItem('tasks', JSON.stringify(tasks))    
+
+});
 
