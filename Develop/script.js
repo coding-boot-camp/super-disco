d7b038469ca7efd20c7f8605e7c013ebb32a8af2
Champagnest <mchampagned@protonmail.com>
2022-04-19 15:50:41 -0700

    inital commit

---
+++ Develop/script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    $( ".saveBtn" ).click(function() {
+        var value = $( this ).siblings(".description").val();
+        var time = $( this ).parent().attr("id");
+        localStorage.setItem(time, value)
+        console.log(value)
+        console.log(time)
+        
+      });
+
+      
+    console.log( "ready!" );
+});
