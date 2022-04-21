6c3f06adb09ebec8b94d8d484db14d7e30188155
Champagnest <mchampagned@protonmail.com>
2022-04-21 12:16:37 -0700

    minor changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,16 @@
-import moment from 'moment';
-
+$("currentTime").text(date.format("LLL"));
 $( document ).ready(function() {
-    $( ".saveBtn" ).click(function() {
-        var value = $( this ).siblings(".description").val();
-        var time = $( this ).parent().attr("id");
-        localStorage.setItem(time, value)
-        console.log(value)
-        console.log(time)
-        
-      });
+  $(".saveBtn").click(function() {
+    console.log("Here, clicked");
+    var localStorageVariable = localStorage.getItem(time);
+console.log(localStorageVariable)
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, value)
+    console.log(value)
+    console.log(time)
+    
+});
 
       
     console.log( "ready!" );
