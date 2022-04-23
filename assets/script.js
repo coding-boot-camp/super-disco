baf577e5cff6578611ab6920894b88758e5f474c
Hunter Nelson <huntknelson@gmail.com>
2022-04-23 13:30:01 -0500

    fell asleep

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,16 @@
+let sched = {}
 // This posts date/time in the header
 const today = moment().format('MMMM Do YY, H:mm:ss a')
 $("#currentDay").html(today);
 
+// local storage
+const loadSched = () => {
+    sched = JSON.parse(localStorage.getItem("sched"))
+   }
+
+
+// save button 
+$(".btn").click(function(){
+    console.log('CLICK')
+    // saveSched();
+});
