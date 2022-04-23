81336f48a90affbe5fc4e03b1b8baac9255bc104
Hunter Nelson <huntknelson@gmail.com>
2022-04-23 14:51:38 -0500

    created iterating function to pull from localStorage

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,18 @@ $(document).ready(function() {
     const today = moment().format('MMMM Do YY, H:mm:ss a')
     $("#currentDay").html(today);
 
+    // local Storage function
+    loadStorage = () => {
+        // create an array for each row parent id
+        const id = ["9","10","11","12","13","14","15","16","17"]
+        // iterate through entire id list
+        for (let i = 0; i < id.length; i++) {
+        // creates an id element with a value of each of the id iterations
+            const idEl = id[i];
+        // sets the value of each description to locally saved data of each corresponding id
+            $(`#${idEl} .description`).val(localStorage.getItem(idEl))
+        }
+    }
 
     // Saves to local storage on button click
     $(".btn").click(function() {
@@ -13,13 +25,6 @@ $(document).ready(function() {
         // saves hour and description to localStorage
         localStorage.setItem(hour,description)
     });
-    //   // Load Storage
-      $('#9 .description').val(localStorage.getItem(9))
-    console.log(localStorage.getItem("9"))
 
+    loadStorage();
 });
-
- // wip more effecient loadstorage
- // const loadStorage = () => {
-    //     const id = ["9","10","11","12","13","14","15","16","17"]
-    // }
