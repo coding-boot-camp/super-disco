f4803a5cb16069deb93a6d80228b78e6764bbac1
Hunter Nelson <huntknelson@gmail.com>
2022-04-23 17:00:39 -0500

    got bg change working

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,23 @@
-
 $(document).ready(function() {
     // This posts the current date and 24h clock in the header
     const today = moment().format('MMMM Do YY, H:mm:ss a')
     $("#currentDay").html(today);
 
     loadLocal = () => {
-        // create an array for each row parent id
-        const id = ["9","10","11","12","13","14","15","16","17"]
+        // creates an element to run through for each row.id
+        let id = ["9","10","11","12","13","14","15","16","17"]
         // iterate through entire id list
         for (let i = 0; i < id.length; i++) {
         // creates an id element with a value of each of the id iterations
-            const idEl = id[i];
+            let idEl = id[i];
         // sets the value of each description to locally saved data of each corresponding id
             $(`#${idEl} .description`).val(localStorage.getItem(idEl))
         }
     };
+    loadLocal();
 
     // Saves to local storage on button click
-    $(".btn").click(function() {
+    $(".btn").click(() => {
         // pulls hour from id of .btn parent div
         let hour = $(this).parent().attr("id")
         // pulls description from text value of textarea
@@ -25,16 +25,28 @@ $(document).ready(function() {
         // saves hour and description to localStorage
         localStorage.setItem(hour,description)
     });
-
-    loadLocal();
-
-    // Tracks time and changes colors of "row"
+    // tracks time and shifts row color
     timeTracker = () => {
-        let time = moment().hours()
+        // loops through the .row DOM element activating the function
+        $(".row").each(function() {
+            // current hour from moment js
+            let currentTime = moment().hours();
+            // pulls interger from id of each .row looped through
+            let timeBlock = parseInt($(this).attr("id"));
+            if(timeBlock > currentTime) {
+                $(this).addClass("future")
+                $(this).removeClass("present")
 
-        console.log(time);
+            }else if(timeBlock == currentTime) {
+                $(this).removeClass("future")
+                $(this).removeClass("past")
+                $(this).addClass("present")
+            } else {
+                $(this).removeClass("future")
+                $(this).removeClass("present")
+                $(this).addClass("past")
+            }
+        })
     }
-
     timeTracker();
-
 });
