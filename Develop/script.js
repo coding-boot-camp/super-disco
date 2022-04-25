b901007dff674a9c0b6a93fcc3ab53d821c3e691
Champagnest <mchampagned@protonmail.com>
2022-04-25 13:10:09 -0700

    trying to get time and color coding to work

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,13 @@
-$("currentTime").text(date.format("LLL"));
+var todaysDate = moment().format("MMMM Do YYYY,h:mm a");
+$("#currentDay").text(todaysDate);
+
+var scheduleObj = {};
+
 $( document ).ready(function() {
-  $(".saveBtn").click(function() {
+  timeCheck();
+  loadEvents();
+
+  $(".saveBtn").click(function(event) {
     console.log("Here, clicked");
     var localStorageVariable = localStorage.getItem(time);
 console.log(localStorageVariable)
@@ -15,3 +22,32 @@ console.log(localStorageVariable)
       
     console.log( "ready!" );
 });
+
+function timeCheck() {
+  var currentHour = moment().hour();
+  $(".time-block").each(function () {
+    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+    if (blockTime < currentHour) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (blockTime === currentHour) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past")
+      $(this).removeClass("present")
+      $(this).addClass("future")
+    }
+  });
+}
+
+function loadEvents() {
+  scheduleObj = JSON.parse(localStorage.getItem("schedule")) || {};
+  $.each(scheduleObj, function (time, description){
+    var scheduleKey.querySelector(".description").textContent = description;
+  });
+
+};
